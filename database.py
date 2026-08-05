a20b4788cf04444b1350663e91c2cba854551448
DeekshithNetz <deekshithnetz@gmail.com>
2026-08-05 19:20:25 +0530

    basic

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./notification.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
--- firebase_service.py
+++ firebase_service.py
@@ -1,9 +1,18 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("firebase_key.json")
+if os.path.exists("firebase_key.json"):
+    # Local development
+    cred = credentials.Certificate("firebase_key.json")
+else:
+    # Render
+    firebase_credentials = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(firebase_credentials)
 
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 
 def send_push_notification(token: str, title: str, body: str):
@@ -16,5 +25,4 @@ def send_push_notification(token: str, title: str, body: str):
     )
 
     response = messaging.send(message)
-    print(response)
     return response
